701c5a9dc76abcf4f0412a00bb02bd66abd21b39
Tenzin <tenthinten@gmail.com>
2022-02-18 22:23:27 +0530

    form and input and search biutton

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ const App = () => {
 
   const APP_ID = '642a9d8a';
   const API_KEY = "8910b1b2edd7ae5fd6d04d5f18c8175f";
-  const exampleReq = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}";
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`;
 
   return(
     <div className="App">
-      <h1>Hello</h1>
+      <form>
+        <input type="text" />
+        <button type="submit">Search</button>
+      </form>
     </div>
   )
 }
