dc8dff89dc19377a1530d11b0db7175f8e0daba4
Tenzin <tenthinten@gmail.com>
2022-02-18 22:32:37 +0530

    counter number increase

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect,useState } from 'react';
 
 const App = () => {
 
   const APP_ID = '642a9d8a';
   const API_KEY = "8910b1b2edd7ae5fd6d04d5f18c8175f";
   const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`;
+  
+  const [counter,setCounter] = useState(0);
 
+  useEffect(() => {
+    console.log('Effect has been run')
+  });
   return(
     <div className="App">
-      <form>
-        <input type="text" />
-        <button type="submit">Search</button>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button 
+          className="search-button" 
+          type="submit"
+        >
+          Seacrch
+        </button>
       </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   )
 }
