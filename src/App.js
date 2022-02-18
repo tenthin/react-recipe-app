b5ac1207ce90b7225b78dfbd81663c8f28b9b00d
Tenzin <tenthinten@gmail.com>
2022-02-18 22:20:46 +0530

    add api keu and website anme

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import React from 'react';
 const App = () => {
 
   const APP_ID = '642a9d8a';
+  const API_KEY = "8910b1b2edd7ae5fd6d04d5f18c8175f";
+  const exampleReq = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}";
+
   return(
     <div className="App">
       <h1>Hello</h1>
