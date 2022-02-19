0a4e87b95036942b3c2367a2bec4145ae9713b44
Tenzin <tenthinten@gmail.com>
2022-02-19 13:33:30 +0530

    search button works

--- src/App.js
+++ src/App.js
@@ -9,26 +9,31 @@ const App = () => {
   
   const [recipes,setRecipes] = useState([]);
   const [search,setSearch] = useState('');
-
+  const [query,setQuery] = useState('chicken')
 
   useEffect(() => {
     getRecipes();
-  },[]);
+  },[query]);
 
   const getRecipes = async() => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${API_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${API_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
     console.log(data.hits);
   }
 
   const updateSearch = e => {
-    setSearch(e.target.value)
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
   }
 
   return(
     <div className="App">
-      <form className="search-form">
+      <form onSubmit={getSearch} className="search-form">
         <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button 
           className="search-button" 
